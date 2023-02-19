0a904ef0621ea04ab1abc07ab06f80dc2a27cb8a
Your Name <you@example.com>
2023-02-19 10:18:40 -0800

    5-printf.c,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - print a line of code using puts
+ * main - print a c program using printf
  *
  * Return: 0
  */
 int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{
+       printf("with proper grammar, but the outcome is a piece of art,\n");
+       return (0)      
 }	
