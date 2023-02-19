9a428474dfc66886ab3e936aa3941b3a2c5c50bf
Your Name <you@example.com>
2023-02-19 10:55:36 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <Stio.h>
+#include <stdio.h>
 /**
- * main - print a C program using printf
+ * main - print a c program using printf
  *
  * Return: 0
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
-}
+	return (0);				
+}	
