a71c2350a8b654bf166bd79efcca7c1c6fddf92e
Your Name <you@example.com>
2023-02-19 20:35:15 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- * return 1 (Success)
+ *  main - A c program that prints a line to the standard error
+ *  return 1 (Success)
 */
 int main(void)
 {
-	fput("and that piece of art is useful\n" - Dora korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
 	return (1);
 }
