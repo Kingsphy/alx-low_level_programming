19b4093dc3b51913e9f25e3454798c3948b96a1d
Your Name <you@example.com>
2023-03-02 14:43:08 -0800

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -24,7 +24,6 @@ temp = *(n + j);
 /**
 * infinite_add - add 2 numbers together
 * @n1: text representation of 1st number to add
-
 * @n2: text representation of 2nd number to add
 * @r: pointer to buffer
 * @size_r: buffer size
