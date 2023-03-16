9da12609b7b63ecee978894e3baf185bf12d147a
Your Name <you@example.com>
2023-03-16 15:43:16 -0700

    C - malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,60 +1,36 @@
-#include <stdlib.h>
 #include "main.h"
-int word_len(char *str);
-int count_word(char *str);
-char **strtow(char *str);
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
-* word_len - locates the index marking the end of the
-* first word contained within a string.
-* @str: the string to be searched.
-*
-* return: the index marking the end of the initial word pointed to by str.
-*/
-int word_len(char *str)
+ * print_tab - Prints an array of string
+ * @tab: The array to print
+ *
+ * Return: nothing
+ */
+void print_tab(char **tab)
 {
-int index = 0, len = 0;
-while (*(str + index) && *(str + index) != ' ')
-{
-len++;
-index++;
-}
-return (len);
+    int i;
+
+    for (i = 0; tab[i] != NULL; ++i)
+    {
+        printf("%s\n", tab[i]);
+    }
 }
 /**
-* count_words - count the number of words contained within a string.
-* @str: the string to be searched.
-*
-* return: the number of words contained within str
-*/
-char **strtow(char *str)
+ * main - check the code for ALX School students.
+ *
+ * Return: 1 if an error occurred, 0 otherwise
+ */
+int main(void)
 {
-char **strings;
-int index = 0, words, w, letters, l;
-if (str == NULL || str(0) == '\0')
-return (NULL);
-words = count_words(str);
-if (words == 0)
-return (NULL)
-strings = malloc(sizeof(char *) * (words + 1));
-if (strings == NULL)
-return (NULL);
-for (w = 0; w < words; w++)
+char **tab;
+tab = strtow("      ALX School         #cisfun      ");
+if (tab == NULL)
 {
-while (str[index] == ' ')
-index++;
-letters = words_len(str + index);
-strings(w) = malloc(sizeof(char) = (letter + 1));
-if (strings[w] == NULL)
-{
-for (; w >= 0; w--)
-free(strings[w])
-free(strings);
-return (NULL);
+printf("Failed\n");
+return (1);
 }
-for (l = 0; l < leters; l++)
-strings[w][l] = str[index++];
-strings[w][l] = '\0';
-}
-strings[w] = NULL;
-return (strings);
+print_tab(tab);
+return (0);
 }
